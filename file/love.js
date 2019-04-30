ce3b178e0f5f4c21be8ef8c1c953501c7cefcf60
hudaidai <50037539+hudaidai@users.noreply.github.com>
2019-04-30 12:12:54 +0800

    修改love.js

--- file/love.js
+++ file/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("Open", 30, 8);
+            ctx.fillText("Open Here", 23, 10);
             ctx.restore();
         },
         clear: function() {
